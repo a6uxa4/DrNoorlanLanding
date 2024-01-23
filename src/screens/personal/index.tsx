f2fa84d2c39f05570f5b99d4217c034180ee9531
Abu <104144116+a6uxa4@users.noreply.github.com>
2024-01-23 15:21:40 +0600

    fix

--- src/screens/personal/index.tsx
+++ src/screens/personal/index.tsx
@@ -1,11 +1,6 @@
 "use client";
 
-import ContainerSlider from "@/components/UI/Containerslider";
-import { PersonalCard } from "@/components/personal/PersonalCard";
 import { useSectionInView } from "@/hooks/useSectionInView";
-import { PERSONALS } from "@/utils/constants/personal.constant";
-import React from "react";
-import { SwiperSlide } from "swiper/react";
 
 export const PersonalPage = () => {
   const { ref } = useSectionInView("Врачи");
@@ -17,22 +12,8 @@ export const PersonalPage = () => {
           Наши доктора
         </h1>
       </div>
-      <div className="w-full max-w-[100vw] m-auto hidden xl:items-center gap-5 xl:justify-center xl:block">
-        <ContainerSlider cardWidth={300}>
-          {PERSONALS.map((item) => (
-            <SwiperSlide
-              key={item.id}
-              className="w-full mt-[50px] !flex !items-center !justify-center"
-            >
-              <PersonalCard personal={item} />
-            </SwiperSlide>
-          ))}
-        </ContainerSlider>
-      </div>
-      <div className="w-full items-center justify-center max-w-[1440px] m-auto flex gap-5 mt-[70px] xl:hidden">
-        {PERSONALS.map((item) => (
-          <PersonalCard key={item.id} personal={item} />
-        ))}
+      <div className="w-full max-w-[100vw] m-auto">
+
       </div>
     </div>
   );
